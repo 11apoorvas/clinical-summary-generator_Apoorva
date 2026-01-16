2e746e2c20dd0a8312b017a924f3bbf802e19bad
11apoorvas <44433458+11apoorvas@users.noreply.github.com>
2026-01-16 06:01:19 -0800

    little change

--- llm.py
+++ llm.py
@@ -1,16 +1,4 @@
-# import os
-# import json
-# from openai import OpenAI
 
-# client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
-
-# def generate_summary(prompt: str) -> dict:
-#     response = client.chat.completions.create(
-#         model="gpt-4o-mini",
-#         messages=[{"role": "user", "content": prompt}],
-#         temperature=0.2
-#     )
-#     return json.loads(response.choices[0].message.content)
 
 def generate_summary(prompt: str) -> dict:
     """
